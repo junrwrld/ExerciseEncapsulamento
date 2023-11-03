cf8ac089a79dce71dd962a7b78d06e416b47cc7f
João Junior <batistasousajoao@gmail.com>
2023-11-03 19:36:28 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciseEncapsulamento/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using ExerciseEncapsulamento;
+using System.Globalization;
+
+namespace Exercise
+{
+    public class ExempleOne
+    {
+        static void main(String[] args)
+        {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial? (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' && resp == 'S')
+            {
+                Console.WriteLine("Entre com o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+
+
+        }
+    }
+}
